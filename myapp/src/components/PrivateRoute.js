05d2a4604bd6e4650e921e1bcb4cb81ad0882dd2
Shireesha <shireeshakalwala@gmail.com>
2026-02-04 14:03:39 +0530

    onboarding_url

--- myapp/src/components/PrivateRoute.js
+++ myapp/src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem("authToken"); // adjust if needed
+  const isAuthenticated = !!localStorage.getItem("token");
 
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
